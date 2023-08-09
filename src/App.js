72b3cacd23ee1d0ce84d1905201d9c3e186d8e47
Nicholas Williams <williamsnick606@gmail.com>
2023-08-08 22:14:19 -0700

    put the window size behavior inside the Navbar component instead of in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import { useState } from 'react';
 import Navbar from './components/Navbar.js';
 
 const App = () => {
-	const [width, setWidth] = useState(0);
-	const [height, setHeight] = useState(0);
-  
-	window.addEventListener('load', () => {
-	  setWidth(window.innerWidth);
-	  setHeight(window.innerHeight);
-	});
-
-	window.addEventListener('resize', () => {
-		setWidth(window.innerWidth);
-		setHeight(window.innerHeight);
-	  });
   
 	return (
 	  <div>
-		<Navbar width={width} height={height} />
+		<Navbar />
 		<h2>
 			conditional_render_window_size React App
 		</h2>
-		<p>The window width is {width}px.</p>
-		<p>The window height is {height}px.</p>
 	  </div>
 	);
   };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import About from './About.js';
 import Posts from './Posts.js';
 
+let Navbar = () => {
 
-let Navbar = (props) => {
-	let winWidth = props.width
-	let winHeight = props.height
+
+	let [width, setWidth] = useState(0);
+	let [height, setHeight] = useState(0);
+  
+	window.addEventListener('load', () => {
+	  setWidth(window.innerWidth);
+	  setHeight(window.innerHeight);
+	});
+
+	window.addEventListener('resize', () => {
+		setWidth(window.innerWidth);
+		setHeight(window.innerHeight);
+	  });
 
 	return(
 		<div>
@@ -12,11 +24,11 @@ let Navbar = (props) => {
 				Navbar now ...
 			</h4>
 			<h3>
-				Hi1 da Window width is {winWidth}.
+				Hi1 da Window width is {width}.
 			</h3>
 
 			<h3>
-				Hi2 da Window height is {winHeight}.
+				Hi2 da Window height is {height}.
 			</h3>
 		</div>
 	// rest of the code..
